ea797de1561defd1fc08966065be8935e11425ec
italex10 <italex10@gmail.com>
2025-11-29 16:04:33 +0000

    Update mobile_agent.py

--- mobile_agent.py
+++ mobile_agent.py
@@ -65,7 +65,7 @@ if start_btn and api_key and meeting:
         )
 
         model = genai.GenerativeModel(
-            model_name='gemini-1.5-pro',
+            model_name='gemini-1.5-flash',
             tools=tools,
             generation_config=generation_config,
             system_instruction=f"""
